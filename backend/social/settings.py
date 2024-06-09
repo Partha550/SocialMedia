641016a6b0eb85af4734dc8daa915c3529e193b5
Partha Samanta <parthasamanta@IABLR-LT396.local>
2024-06-09 12:45:26 +0530

    Set Pagination

--- backend/social/settings.py
+++ backend/social/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     #  "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     #  "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 100,
+    "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
